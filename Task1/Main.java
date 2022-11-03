de031387b6f56b90bca451d21df71255034f82cc
Alan <100705149+theMesox@users.noreply.github.com>
2022-11-03 22:14:53 +0100

    Added class method and put everything in one file

--- Task1/Main.java
+++ Task1/Main.java
@@ -1,17 +1,34 @@
 package Task1;
 
+final class Owner {
+    String name;
+    int age;
+}
+
+final class Car {
+    String make;
+    String model;
+    int year;
+    Owner car_owner;
+
+    void message() {
+        System.out.println("Owner's name is " + car_owner.name + " and the car they have is " + make + " " + model);
+        System.out.println("Owner's age is " + car_owner.age + " and the car they have is from " + year);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+        Owner owner = new Owner();
+        owner.name = "Mr. X";
+        owner.age = 35;
+        
         Car car = new Car();
         car.make = "Mazda";
         car.model = "CX-5";
         car.year = 2018;
+        car.car_owner = owner;
 
-        Owner owner = new Owner();
-        owner.name = "Mr. X";
-        owner.age = 35;
-
-        System.out.println("Owner's name is " + owner.name + " and the car they have is " + car.make + " " + car.model);
-        System.out.println("Owner's age is " + owner.age + " and the car they have is from " + car.year);
+        car.message();
     }
 }
