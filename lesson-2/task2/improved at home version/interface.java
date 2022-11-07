8eeafdda359aedb737df690c88ad7f9812918579
Alan <100705149+theMesox@users.noreply.github.com>
2022-11-07 23:00:33 +0100

    Expanded fuel messages

--- lesson-2/task2/improved at home version/interface.java
+++ lesson-2/task2/improved at home version/interface.java
@@ -3,42 +3,41 @@ package Task2;
 interface Vehicle {
     Vehicle accelerate(int factor);
     Vehicle brake(int factor);
-    //final int factor = 50;
     Vehicle fuel_level();
     Vehicle final_distance();
-    //int MAXFUEL = 60;
-    //String ERORRMSG = "Fuel tank is full!";
 }
 
 class Car implements Vehicle {
-    private int speed;
+    private int current_speed;
     private int distance;
     private int fuel_level;
 
-    Car (int speed, int distance, int fuel_level) {
-        this.speed = speed;
+    Car (int current_speed, int distance, int fuel_level) {
+        this.current_speed = current_speed;
         this.distance = distance;
         this.fuel_level = fuel_level;
     }
     @Override
     public Vehicle accelerate(int factor) {
-        this.speed += factor;
-        System.out.println("Accelerating the car by " + this.speed);
-        return new Car(this.speed, distance, fuel_level);
+        this.current_speed += factor;
+        System.out.println("Accelerating the car by " + this.current_speed);
+        return new Car(this.current_speed, distance, fuel_level);
     }
 
     public Vehicle brake(int factor) {
-        System.out.println("Braking the car by " + this.speed);
-        this.speed -= factor;
+        System.out.println("Braking the car by " + this.current_speed);
+        this.current_speed -= factor;
         this.distance += factor;
         this.fuel_level -= factor;
-        return new Car(this.speed, this.distance, this.fuel_level);
+        return new Car(this.current_speed, this.distance, this.fuel_level);
     }
     
     public Vehicle fuel_level() {
         System.out.println("Current fuel level of the car: " + this.fuel_level);
-        if (fuel_level <= 10) {
+        if ((this.fuel_level <= 10) && (this.fuel_level >= 1)) {
             System.out.println("Low amount of fuel!");
+        } else if (this.fuel_level == 0) {
+            System.out.println("No fuel!");
         }
         return null;
     }
@@ -50,33 +49,35 @@ class Car implements Vehicle {
 } 
 
 class Truck implements Vehicle {
-    private int speed;
+    private int current_speed;
     private int distance;
     private int fuel_level;
-    Truck (int speed, int distance, int fuel_level) {
-        this.speed = speed;
+    Truck (int current_speed, int distance, int fuel_level) {
+        this.current_speed = current_speed;
         this.distance = distance;
         this.fuel_level = fuel_level;
     }
     @Override
     public Vehicle accelerate(int factor) {
-        this.speed += factor;
-        System.out.println("Accelerating the truck by " + this.speed);
-        return new Truck(this.speed, this.distance, fuel_level);
+        this.current_speed += factor;
+        System.out.println("Accelerating the truck by " + this.current_speed);
+        return new Truck(this.current_speed, this.distance, fuel_level);
     }
 
     public Vehicle brake(int factor) {
-        System.out.println("Braking the truck by " + this.speed);
-        this.speed -= factor;
+        System.out.println("Braking the truck by " + this.current_speed);
+        this.current_speed -= factor;
         this.distance += factor;
         this.fuel_level -= factor;
-        return new Truck(this.speed, this.distance, this.fuel_level);
+        return new Truck(this.current_speed, this.distance, this.fuel_level);
     }
 
     public Vehicle fuel_level() {
         System.out.println("Current fuel level of the truck: " + this.fuel_level);
-        if (this.fuel_level <= 10) {
+        if ((this.fuel_level <= 10) && (this.fuel_level >= 1)) {
             System.out.println("Low amount of fuel!");
+        } else if (this.fuel_level == 0) {
+            System.out.println("No fuel!");
         }
         return null;
     }
