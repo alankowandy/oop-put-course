0b9f7caf1c46937001824477ea9fc4a26fdc8aea
Alan <100705149+theMesox@users.noreply.github.com>
2022-12-10 14:55:40 +0100

    Add files via upload

---
+++ lesson-3/task2_1.java
@@ -0,0 +1,28 @@
+public class Comparisons {
+    private int a, b;
+
+    Comparisons (int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    
+    public int max(){
+        return a > b ? a : b;
+    }
+
+    public int min(){
+        return a < b ? a : b;
+    }
+
+    public float avg(){
+        return (a + b) / 2.0f;
+    }
+
+}
+
+public static void main(String[] args) {
+    Comparisons comparisons = new Comparisons(1, 2);
+    System.out.println("Max: " + max());
+    System.out.println("Min: " + min());
+    System.out.println("Avg: " + avg());
+}
