886110cd35caa0f5ba87cda685326da0332568c6
Alan <100705149+theMesox@users.noreply.github.com>
2023-01-26 22:06:48 +0100

    Add files via upload

--- src/pl/poznan/put/GameClasses/Branches/SecondUnknown.java
+++ src/pl/poznan/put/GameClasses/Branches/SecondUnknown.java
@@ -6,10 +6,11 @@ final class SecondUnknownDetails implements Details {
 
     @Override
     public Details location(String place) throws InterruptedException {
-        GameMechanics.slowPrint(30,"Current location: ");
-        GameMechanics.pause(1500);
-        GameMechanics.slowPrint(30,place + "\n");
-        GameMechanics.pause(1000);
+        GameMechanics gameMechanics = new GameMechanics();
+        gameMechanics.slowPrint(30,"Current location: ");
+        gameMechanics.pause(1500);
+        gameMechanics.slowPrint(30,place + "\n");
+        gameMechanics.pause(1000);
         return null;
     }
 }
@@ -19,17 +20,18 @@ public class SecondUnknown {
         Details details = new SecondUnknownDetails();
         System.out.println("\n------------------------------------------------------------------\n");
         details.location("Unknown");
-        GameMechanics.slowPrint(30,"\nI run to the car to look for the other person.\n");
-        GameMechanics.promptEnterKey();
-        GameMechanics.slowPrint(30,"I'm sure I wasn't alone. I just can't remember who was it.\n");
-        GameMechanics.promptEnterKey();
-        GameMechanics.slowPrint(30,"In the car I only find huge mess and blood marks.\n");
-        GameMechanics.promptEnterKey();
-        GameMechanics.slowPrint(50,"I remember now...\n");
-        GameMechanics.promptEnterKey();
-        GameMechanics.slowPrint(30,"Where is she...\n");
-        GameMechanics.pause(2000);
-        GameMechanics.slowPrint(40,"\nThe sound of sirens is really close now.\n");
-        GameMechanics.slowPrint(200,"\n...");
+        GameMechanics gameMechanics = new GameMechanics();
+        gameMechanics.slowPrint(30,"\nI run to the car to look for the other person.\n");
+        gameMechanics.promptEnterKey();
+        gameMechanics.slowPrint(30,"I'm sure I wasn't alone. I just can't remember who was it.\n");
+        gameMechanics.promptEnterKey();
+        gameMechanics.slowPrint(30,"In the car I only find huge mess and blood marks.\n");
+        gameMechanics.promptEnterKey();
+        gameMechanics.slowPrint(50,"I remember now...\n");
+        gameMechanics.promptEnterKey();
+        gameMechanics.slowPrint(30,"Where is she...\n");
+        gameMechanics.pause(2000);
+        gameMechanics.slowPrint(40,"\nThe sound of sirens is really close now.\n");
+        gameMechanics.slowPrint(200,"\n...");
     }
 }
